e8b5c5401d285866d6f0eb13cc225ff224eaa435
parham babaei <marshparham2@gmail.com>
2026-07-08 14:45:57 +0330

    Update urls.py

--- aura/urls.py
+++ aura/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('create-room/', views.create_room, name='create_room'),
     path('update-profile/', views.update_profile, name='update_profile'),
     path('admin-panel/', views.admin_panel, name='admin_panel'),
+    path('google123456789.html', views.google_verification, name='google_verification'),
 ]
